905c922cc5ceaacf1152eec1e067f23d2d28cb77
kxxt <rsworktech@outlook.com>
2022-02-15 08:33:27 +0800

    dep: use @wojtekmaj/enzyme-adapter-react-17.

--- __tests__/react-typed.test.js
+++ __tests__/react-typed.test.js
@@ -2,7 +2,7 @@ import 'raf/polyfill';
 import React from 'react';
 import Typed from '../src';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({adapter: new Adapter()});
 
