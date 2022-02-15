987f1d08b09b1d6239c83c1f6cc6c569bed0fa5b
kxxt <rsworktech@outlook.com>
2022-02-15 14:34:48 +0800

    fix: a storybook warning.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,8 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-controls",
     "@storybook/addon-essentials",
+    "@storybook/addon-controls",
     "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
